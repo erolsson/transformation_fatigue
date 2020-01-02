45116ce56f4d5ec1eabf2a06b49572b23c6a8b41
erolsson@kth.se <erolsson@kth.se>
2020-01-02 12:40:00 +0100

    "changed material name in write_rolling_input_file.py"

--- rolling_contact/write_rolling_input_file.py
+++ rolling_contact/write_rolling_input_file.py
@@ -71,6 +71,8 @@ def create_roller_model(simulation_file_name, geometry_file_name, material, p0, 
 
     file_lines.extend(material.material_input_file_string())
     q = rolling_angle/2*np.pi/180
+    file_lines.append('*Initial Conditions, type=Solution, user')
+    file_lines.append('*Initial Conditions, type=Stress, user')
     file_lines.append('*Assembly, name=rolling_contact_model')
     overlap = 0.03
     d = 20.1 + (20.1*(1 - np.cos(q)) - overlap)*np.cos(q)
