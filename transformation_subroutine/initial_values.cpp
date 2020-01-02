12808255bd0a867b0134bc9468ffb7ecd670fad6
erolsson@kth.se <erolsson@kth.se>
2020-01-02 17:03:50 +0100

    "changed material name in write_rolling_input_file.py"

--- transformation_subroutine/initial_values.cpp
+++ transformation_subroutine/initial_values.cpp
@@ -84,13 +84,14 @@ std::size_t reorder_gauss_pt(std::size_t gp, std::string part_name) {
 
 std::pair<std::size_t, std::string> user_model_data(int noel) {
     int user_elem_number = 0;
-    char out_char[256];
+    char out_char[80];
     int out_len = 0;
     int err = 0;
     {
         std::lock_guard<std::mutex> lock(part_info_mutex);
         getpartinfoc_(out_char, out_len, noel, 1, user_elem_number, err);
     }
+    std::cout << out_char << std::endl;
     std::cout << std::string(out_char, out_char+out_len) << " " << user_elem_number << std::endl;
     return std::make_pair(user_elem_number, std::string(out_char, out_char+out_len));
 }
