a47fd4f923f0faf5d3611c62bb44f8b4765547be
erolsson@kth.se <erolsson@kth.se>
2020-01-05 20:38:26 +0100

    "changed material name in write_rolling_input_file.py"

--- transformation_subroutine/transformation_umat.cpp
+++ transformation_subroutine/transformation_umat.cpp
@@ -22,7 +22,7 @@ const double pi = 3.14159265359;
 std::mutex print_mutex;
 
 void print_at_time(const std::string msg, double time, unsigned noel, unsigned npt) {
-    if (time > -1.) {
+    if (time > 0.99) {
         {
             std::lock_guard<std::mutex> lock(print_mutex);
             std::cout << "Elem: " << noel << " gp: " << npt << " " << msg << std::endl;
