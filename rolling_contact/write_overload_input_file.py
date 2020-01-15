1b4f347db0223d44319cb15ace962e13155775cb
erolsson@kth.se <erolsson@kth.se>
2020-01-15 10:05:58 +0100

    "changed material name in write_rolling_input_file.py"

--- rolling_contact/write_overload_input_file.py
+++ rolling_contact/write_overload_input_file.py
@@ -160,7 +160,7 @@ def create_roller_model(simulation_file_name, geometry_file_name, material, p0, 
 
 
 if __name__ == '__main__':
-    heat_treatment_directory = os.path.expanduser('~/rolling_contact/1/')
+    heat_treatment_directory = os.path.expanduser('~/rolling_contact/2/')
     for temp in [-20, 0., 22, 90]:
         for overload in [2., 2.5, 3.]:
             simulation_directory = os.path.expanduser('~/rolling_contact/mechanical_FEM/' + str(temp) + 'C_'
