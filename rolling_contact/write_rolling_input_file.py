d87ecc9644a359bef0fef227d46c99b635a32b6a
erolsson@kth.se <erolsson@kth.se>
2020-01-21 12:10:34 +0100

    "changed material name in write_rolling_input_file.py"

--- rolling_contact/write_rolling_input_file.py
+++ rolling_contact/write_rolling_input_file.py
@@ -177,5 +177,4 @@ if __name__ == '__main__':
         os.makedirs(simulation_directory)
 
     model_file = os.path.expanduser('~/python_fatigue/rolling_contact/input_files/roller.inp')
-    create_roller_model(simulation_directory + 'roller_model_no_trans.inp', model_file, 2000, 7)
     create_roller_model(simulation_directory + 'roller_model.inp', model_file, 2000, 7)
