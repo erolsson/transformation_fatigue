1d11d99a1874db5544cc7f0585fce0f15b8e788e
erolsson@kth.se <erolsson@kth.se>
2020-01-28 16:25:31 +0100

    "changed material name in write_rolling_input_file.py"

--- rolling_contact/evaluate_findley_stress.py
+++ rolling_contact/evaluate_findley_stress.py
@@ -99,7 +99,7 @@ def perform_effective_stress_analysis(residual_stress_odb, mechanical_data, effe
 
 
 if __name__ == '__main__':
-    temperature = '22C'
+    temperature = 'no_trans'
     overload = '2_5GPa'
     reader = InputFileReader()
     reader.read_input_file(os.path.expanduser('~/python_fatigue/rolling_contact/input_files/roller.inp'))
