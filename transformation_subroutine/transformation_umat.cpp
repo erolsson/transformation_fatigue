6882fafdd5bcfb2ef4da5bc2f90c44c9b43c7bd2
erolsson@kth.se <erolsson@kth.se>
2020-07-16 13:34:29 +0200

    "Added model for utmis specimens"

--- transformation_subroutine/transformation_umat.cpp
+++ transformation_subroutine/transformation_umat.cpp
@@ -437,6 +437,9 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
                 print_for_position("sigma2: ", sigma_2.transpose().format(CleanFmt), noel, npt);
                 print_for_position("Sigma1: ", Sigma1, noel, npt);
                 print_for_position("Sigma2: ", Sigma, noel, npt);
+                print_for_position("yield_func: ", yield_function(sigma_t, state.total_back_stress(), sy, params),
+                        noel, npt);
+                print_for_position("sigma_t: ", sigma_t.transpose().format(CleanFmt), noel, npt);
                 return;
             }
         }
