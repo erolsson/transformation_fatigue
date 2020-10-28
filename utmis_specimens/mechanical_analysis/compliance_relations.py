f1085c59772fd0496478b32ef117e07d3e27a459
erolsson@kth.se <erolsson@kth.se>
2020-10-28 13:25:26 +0100

    ""

--- utmis_specimens/mechanical_analysis/compliance_relations.py
+++ utmis_specimens/mechanical_analysis/compliance_relations.py
@@ -1,24 +1,28 @@
 import os
 
 from mechanical_analysis_functions import write_mechanical_input_files, Simulation, Step, write_run_file
-from transformation_fatigue.materials.materials import SS2506
+from transformation_fatigue.materials.materials import SS2506_no_trans as SS2506
 
 
 def main():
     max_nominal_stress = 1000
     Wb = 2*5**2/6
     M = max_nominal_stress*Wb
-    steps = [Step(name='loading', load=M, max_inc=0.01)]
-    simulations = [Simulation(name='loading', steps=steps, mode='force')]
+    steps_smooth = [Step(name='loading', load=M, max_inc=0.01)]
+    steps_notched = [Step(name='loading', load=M/2, max_inc=0.01)]
+    simulations = {"smooth": [Simulation(name='loading', steps=steps_smooth, mode='force')],
+                   "notched": [Simulation(name='loading', steps=steps_notched, mode='force')]}
     for specimen in ['smooth', 'notched']:
         geom_filename = os.path.expanduser('~/python_projects/python_fatigue/fatigue_specimens/UTMIS/utmis_'
                                            + specimen + '/utmis_' + specimen + '.inc')
         simulation_directory = os.path.expanduser('~/utmis_specimens/' + specimen + '/mechanical_analysis/')
         if not os.path.isdir(simulation_directory):
             os.makedirs(simulation_directory)
-        job_names = write_mechanical_input_files(specimen, geom_filename, simulation_directory, simulations, SS2506)
-        heat_treatment_data_file = os.path.expanduser('~/utmis_specimens/' + specimen + '/Toolbox_Cooling_utmis_'
-                                                      + specimen + '.htd')
+        job_names = write_mechanical_input_files(specimen, geom_filename, simulation_directory, simulations[specimen],
+                                                 SS2506)
+        heat_treatment_simulation = 'cooltemp_30C_tempering_200C_120min'
+        heat_treatment_data_file = os.path.expanduser('~/utmis_specimens/' + specimen + '/' + heat_treatment_simulation +
+                                                      '/Toolbox_Cooling_utmis_' + specimen + '.htd')
         write_run_file(job_names, heat_treatment_data_file, simulation_directory + '/run_compliance.sh')
 
 
