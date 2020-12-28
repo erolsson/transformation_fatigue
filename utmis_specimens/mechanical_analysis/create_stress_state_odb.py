9b0164535438d59bafccedb08d8869e649af5139
erolsson@kth.se <erolsson@kth.se>
2020-12-28 12:44:41 +0100

    ""

--- utmis_specimens/mechanical_analysis/create_stress_state_odb.py
+++ utmis_specimens/mechanical_analysis/create_stress_state_odb.py
@@ -11,6 +11,7 @@ def main():
     specimen_loads = {'smooth': {-1.: [760 - 70, 760, 760 + 70], 0.: [424. - 26, 424, 424 + 26]},
                       'notched': {-1.: [439 - 20, 439, 439 + 20], 0.: [237. - 16, 237., 237 + 16]}}
     specimen = sys.argv[-1]
+    results_odb_file = os.path.expanduser('~/utmis_specimens/' + specimen + '/stress_state.odb')
     for R in [-1, 0]:
         for load_amplitude in specimen_loads[specimen][R]:
             odb_file_directory = os.path.expanduser('~/utmis_specimens/' + specimen
@@ -18,13 +19,12 @@ def main():
             sim_name = "snom=" + str(int(load_amplitude)) + "_R=" + str(int(R))
             odb_file_name = "/utmis_" + specimen + '_' + sim_name
 
-            results_odb_file = os.path.expanduser('~/utmis_specimens/' + specimen + '/stress_state.odb')
             for instance_name in ['SPECIMEN_PART_NEG', 'SPECIMEN_PART_POS']:
                 s1 = read_field_from_odb('S', odb_file_directory + '/' + odb_file_name + '.odb',
                                          step_name='3_min_load', frame_number=-1, instance_name=instance_name)
 
                 s2 = read_field_from_odb('S', odb_file_directory + '/' + odb_file_name + '.odb',
-                                         step_name='3_min_load', frame_number=-1, instance_name=instance_name)
+                                         step_name='3_max_load', frame_number=-1, instance_name=instance_name)
 
                 s_mean = (s1 + s2)/2
                 s_amp = np.abs(s1 - s2)/2
