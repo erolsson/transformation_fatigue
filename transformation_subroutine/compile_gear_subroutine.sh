8c51d087e30573a8b30c18cf03ea3460a95abe82
erolsson@kth.se <erolsson@kth.se>
2021-07-05 16:47:30 +0200

    ""

--- transformation_subroutine/compile_gear_subroutine.sh
+++ transformation_subroutine/compile_gear_subroutine.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export OLD_LD=${LD_LIBRARY_PATH}
-g++ -c -O3 -std=c++17 -fPIC -I "${EIGENPATH}" -o initial_state_gear.o initial_state_gear.cpp
-g++ -c -O3 -std=c++17 -fPIC -I "${EIGENPATH}" -o transformation_umat.o transformation_umat.cpp
-g++ -c -O3 -std=c++17 -fPIC -I "${EIGENPATH}" -o utilities.o utilities.cpp
+g++-8 -c -O3 -std=c++17 -fPIC -I "${EIGENPATH}" -o initial_state_gear.o initial_state_gear.cpp
+g++-8 -c -O3 -std=c++17 -fPIC -I "${EIGENPATH}" -o transformation_umat.o transformation_umat.cpp
+g++-8 -c -O3 -std=c++17 -fPIC -I "${EIGENPATH}" -o utilities.o utilities.cpp
 gfortran -c -fPIC getpartinfo.f
 ld -r initial_state_gear.o transformation_umat.o getpartinfo.o utilities.o -o gear_subroutine.o
 export LD_LIBRARY_PATH=${OLD_LD}
