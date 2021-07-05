f155937e0a8ea389f10d1ef674c763b76d46873b
erolsson@kth.se <erolsson@kth.se>
2021-07-05 17:22:58 +0200

    ""

--- utmis_specimens/mechanical_analysis/evaluate_findley_stress.py
+++ utmis_specimens/mechanical_analysis/evaluate_findley_stress.py
@@ -113,6 +113,7 @@ def main():
 
         sim_name = "snom=" + str(int(load_amplitude)) + "_R=" + str(int(R))
         odb_file_name = odb_file_directory / ("/utmis_" + specimen + '_' + sim_name + '.odb')
+        print(odb_file_directory)
         print(odb_file_name)
         results_odb_file = (pathlib.Path.home() / "utmis_specimens" / specimen / "mechanical_analysis_relaxed"
                             / "force_control_ra20" / ("findley_" + str(k_750).replace('.', '_') + ".odb"))
